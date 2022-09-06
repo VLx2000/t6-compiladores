e999c19d97cc5ad2a0a60d6968cdbb95b55e7924
eduardo-gualberto <gualberto.se@yahoo.com>
2022-09-06 10:00:40 -0300

    comeco da analise semantica da linguagem

--- src/main/java/br/ufscar/dc/compiladores/TarSemantico.java
+++ src/main/java/br/ufscar/dc/compiladores/TarSemantico.java
@@ -1,5 +1,10 @@
 package br.ufscar.dc.compiladores;
 
+import java.util.ArrayList;
+
+import br.ufscar.dc.compiladores.TarParser.ComprimirContext;
+import br.ufscar.dc.compiladores.TarParser.ExtrairContext;
+
 public class TarSemantico extends TarBaseVisitor<Void> {
 
     @Override
@@ -10,5 +15,25 @@ public class TarSemantico extends TarBaseVisitor<Void> {
     @Override
     public Void visitAcao(TarParser.AcaoContext ctx){            
         return super.visitAcao(ctx);
+    }
+
+    @Override
+    public Void visitComprimir(ComprimirContext ctx) {
+        return super.visitComprimir(ctx);
+    }
+
+    @Override
+    public Void visitExtrair(ExtrairContext ctx) {
+        // TODO Auto-generated method stub
+        ArrayList<String> files = new ArrayList<>();
+        for(var arq: ctx.ARQUIVO()) {
+            String currArq = arq.getText();
+            if(files.contains(currArq)) {
+                TarSemanticoUtils.adicionarErroSemantico(ctx.start, currArq + " apareceu mais de uma vez.");
+            } else {
+                files.add(currArq);
+            }
+        }
+        return super.visitExtrair(ctx);
     }
 }
